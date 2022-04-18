e3e15d28591198330e1db1767d46a8bfbdac16e9
Bishwo <_bish@arjuns-MacBook-Air.local>
2022-04-18 14:12:57 +0545

    for loop program

---
+++ forLoop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int i,j;
+    int arr[10];
+    printf("Enter the numbers  :");
+    for(i=0;i<=10;i++){
+       printf("This is %d sentence\n",i);
+    }
+
+    return 0;
+}
